12aeda057fcbdcad08ad0fc277e7955c481326a2
Yongfu <liao961120@gmail.com>
2021-03-29 15:30:38 +0800

    update

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-pandoc about.md -o about.html --section-divs
+pandoc about.md \
+    -o about.html \
+    --section-divs \
+    --citeproc \
+    --bibliography="references.bib" \
+    --csl="apa.csl" \
+    --metadata link-citations=true \
